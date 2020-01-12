f82f5df86f5a2951d8f0b7c657e5b8c0fc4b4fc0
Your New SJW Waifu <neckbeard@rape.lol>
2020-01-11 18:55:44 -0600

    Change source url

--- mix.exs
+++ mix.exs
@@ -18,10 +18,10 @@ defmodule Pleroma.Mixfile do
       # Docs
       name: "Pleroma",
       homepage_url: "https://pleroma.social/",
-      source_url: "https://git.pleroma.social/pleroma/pleroma",
+      source_url: "https://git.pleroma.social/sjw/pleroma",
       docs: [
         source_url_pattern:
-          "https://git.pleroma.social/pleroma/pleroma/blob/develop/%{path}#L%{line}",
+          "https://git.pleroma.social/sjw/pleroma/blob/develop/%{path}#L%{line}",
         logo: "priv/static/static/logo.png",
         extras: ["README.md", "CHANGELOG.md"] ++ Path.wildcard("docs/**/*.md"),
         groups_for_extras: [
