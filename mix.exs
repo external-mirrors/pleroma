4de4e580dabb021523c416bbbd55f6101ccc3cf1
Your New SJW Waifu <neckbeard@rape.lol>
2020-03-15 01:42:40 -0500

    Nevermind we're mastodon now

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Pleroma.Mixfile do
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: ["coveralls.html": :test],
       # Docs
-      name: "mlady",
+      name: "mastodon",
       homepage_url: "https://pleroma.social/",
       source_url: "https://git.pleroma.social/sjw/pleroma",
       docs: [
