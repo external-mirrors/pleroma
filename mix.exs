0d0fda0f62b15d0da4f98fd571ae34f604c2ab3f
Your New SJW Waifu <neckbeard@rape.lol>
2020-03-15 18:34:02 -0500

    This fucks with apps

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Pleroma.Mixfile do
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: ["coveralls.html": :test],
       # Docs
-      name: "mastodon",
+      name: "Pleroma",
       homepage_url: "https://pleroma.social/",
       source_url: "https://git.pleroma.social/sjw/pleroma",
       docs: [
