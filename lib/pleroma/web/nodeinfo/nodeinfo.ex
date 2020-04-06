416f970aaba59c94093975520513e79520a808af
stwf <steven.fuchs@dockyard.com>
2020-04-06 10:08:06 -0400

    update, fix tests

--- lib/pleroma/web/nodeinfo/nodeinfo.ex
+++ lib/pleroma/web/nodeinfo/nodeinfo.ex
@@ -55,7 +55,8 @@ defmodule Pleroma.Web.Nodeinfo.Nodeinfo do
         end,
         if Config.get([:instance, :safe_dm_mentions]) do
           "safe_dm_mentions"
-        end
+        end,
+        "pleroma_emoji_reactions"
       ]
       |> Enum.filter(& &1)
 
