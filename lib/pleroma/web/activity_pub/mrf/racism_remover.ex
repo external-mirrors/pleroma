d7af3d92e7e14b55b93b1c1b26c2488685670b47
Your New SJW Waifu <neckbeard@rape.lol>
2020-06-25 17:24:36 -0500

    add racism remover mrf

---
+++ lib/pleroma/web/activity_pub/mrf/racism_remover.ex
@@ -0,0 +1,22 @@
+defmodule Pleroma.Web.ActivityPub.MRF.RacismRemover do
+        require Logger
+        @behaviour Pleroma.Web.ActivityPub.MRF
+
+        @impl true
+        def filter(%{"type" => "Delete", "actor" => actor} = object) do
+                actor_info = URI.parse(actor)
+                if(actor_info.host == "neckbeard.xyz") do
+                        Logger.warn("DELETE from NB, not rejecting:  #{inspect(object)}")
+                        {:ok, object}
+                else
+                        Logger.warn("DELETE rejected: #{inspect(object)}")
+                        {:reject, object}
+                end
+        end
+
+        @impl true
+        def filter(object), do: {:ok, object}
+
+        @impl true
+       def describe, do: {:ok, %{}}
+end
