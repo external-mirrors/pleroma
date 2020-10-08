b7d11c094ec15a556ff018a6271e42533673b635
Your New SJW Waifu <neckbeard@rape.lol>
2020-10-07 20:35:45 -0500

    format slashb mrf

--- lib/pleroma/web/activity_pub/mrf/sup_slash_b.ex
+++ lib/pleroma/web/activity_pub/mrf/sup_slash_b.ex
@@ -1,37 +1,41 @@
 defmodule Site.SupSlashB do
-	require Logger
-	@behaviour Pleroma.Web.ActivityPub.MRF
+  require Logger
+  @behaviour Pleroma.Web.ActivityPub.MRF
 
-	@impl true
-	def filter(
-		%{
-			"type" => "Create",
-			"actor" => actor,
-			"object" => %{
-				"type" => "Note",
-				"summary" => "/b/",
-			} = object
-		} = message
-	) do
-                actor_info = URI.parse(actor)
-                if(actor_info.host == "neckbeard.xyz") do
-                        #Logger.warn("sup /b/ #{inspect(object)}")
- 
-			object =
-				object
-				|> Map.put("actor", "https://neckbeard.xyz/users/slash_b")
-			message =
-				message
-				|> Map.put("actor", "https://neckbeard.xyz/users/slash_b")
-				|> Map.put("object", object)
-			{:ok, message}
-                else
-                        {:ok, message}
-                end
-	end
+  @impl true
+  def filter(
+        %{
+          "type" => "Create",
+          "actor" => actor,
+          "object" =>
+            %{
+              "type" => "Note",
+              "summary" => "/b/"
+            } = object
+        } = message
+      ) do
+    actor_info = URI.parse(actor)
 
-	@impl true
-	def filter(message), do: {:ok, message}
+    if(actor_info.host == "neckbeard.xyz") do
+      # Logger.warn("sup /b/ #{inspect(object)}")
+
+      object =
+        object
+        |> Map.put("actor", "https://neckbeard.xyz/users/slash_b")
+
+      message =
+        message
+        |> Map.put("actor", "https://neckbeard.xyz/users/slash_b")
+        |> Map.put("object", object)
+
+      {:ok, message}
+    else
+      {:ok, message}
+    end
+  end
+
+  @impl true
+  def filter(message), do: {:ok, message}
   @impl true
   def describe, do: {:ok, %{}}
 end
