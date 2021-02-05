9fd39458eb6fe8fe215ca4b9d30cfb91c791d92e
Your New SJW Waifu <neckbeard@rape.lol>
2021-02-05 08:48:07 -0600

    fix users

---
+++ fix_users.ex
@@ -0,0 +1,21 @@
+alias Pleroma.{User,Repo}
+
+import Ecto.Query
+import Ecto.Changeset
+
+from(
+  u in User,
+  where: fragment("? ->> 'id' IS NULL", u.info),
+  update: [
+    set: [info: nil]
+  ]
+) |> Repo.update_all([])
+
+User
+|> where([user], fragment("? ->> 'id' IS NULL", user.info))
+|> Repo.all()
+|> Enum.each(fn user ->
+  change(user)
+  |> put_embed(:info, %User.Info{locked: true, hide_followers: true, hide_follows: true})
+  |> User.update_and_set_cache()
+end)
