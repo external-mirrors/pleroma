265b00da107430efaad8787d34cfe9a0288416be
Your New SJW Waifu <neckbeard@rape.lol>
2021-03-19 12:56:39 -0500

    update AntiHellthreadPolicy

--- lib/pleroma/web/activity_pub/mrf/anti_hellthread_policy.ex
+++ lib/pleroma/web/activity_pub/mrf/anti_hellthread_policy.ex
@@ -1,5 +1,5 @@
 # Pleroma: A lightweight social networking server
-# Copyright © 2017-2020 Pleroma Authors <https://pleroma.social/>
+# Copyright © 2017-2021 Pleroma Authors <https://pleroma.social/>
 # SPDX-License-Identifier: AGPL-3.0-only
 
 defmodule Pleroma.Web.ActivityPub.MRF.AntiHellthreadPolicy do
@@ -13,6 +13,9 @@ defmodule Pleroma.Web.ActivityPub.MRF.AntiHellthreadPolicy do
   defp is_block_or_unblock(%{"type" => "Block", "object" => object}),
     do: {true, "blocked", object}
 
+  defp is_block_or_unblock(%{"type" => "Block", "object" => object}),
+       do: {true, "blocked", object}
+
   defp is_block_or_unblock(%{
          "type" => "Undo",
          "object" => %{"type" => "Block", "object" => object}
@@ -24,22 +27,22 @@ defmodule Pleroma.Web.ActivityPub.MRF.AntiHellthreadPolicy do
   defp is_remote_or_displaying_local?(%User{local: false}), do: true
 
   defp is_remote_or_displaying_local?(_),
-    do: Config.get([:mrf_anti_hellthread_policy, :display_local])
+    do: Pleroma.Config.get([:mrf_anti_hellthread_policy, :display_local])
 
   @impl true
   def filter(message) do
+
     with {true, action, object} <- is_block_or_unblock(message),
          %User{} = actor <- User.get_cached_by_ap_id(message["actor"]),
          %User{} = recipient <- User.get_cached_by_ap_id(object),
          true <- recipient.local,
          true <- is_remote_or_displaying_local?(actor),
          false <- User.blocks_user?(recipient, actor) do
-      bot_user = Config.get([:mrf_anti_hellthread_policy, :user])
+      bot_user = Pleroma.Config.get([:mrf_anti_hellthread_policy, :user])
 
       _reply =
         CommonAPI.post(User.get_by_nickname(bot_user), %{
-          "status" =>
-            "@" <> recipient.nickname <> " you have been " <> action <> " by @" <> actor.nickname,
+          status: "@" <> recipient.nickname <> " you have been " <> action <> " by @" <> actor.nickname,
           "visibility" => "direct"
         })
     end
