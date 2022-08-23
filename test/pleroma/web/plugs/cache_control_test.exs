d94f90fd61ae829a75d3490b0e4c6424e92425fe
Tusooa Zhu <tusooa@kazv.moe>
2022-08-22 22:04:44 -0400

    Fix CacheControlTest

--- test/pleroma/web/plugs/cache_control_test.exs
+++ test/pleroma/web/plugs/cache_control_test.exs
@@ -6,6 +6,23 @@ defmodule Pleroma.Web.Plugs.CacheControlTest do
   use Pleroma.Web.ConnCase, async: true
   alias Plug.Conn
 
+  @dir "test/frontend_static_cache_control_test"
+
+  setup do
+    File.mkdir_p!(@dir)
+
+    frontend_path = Path.join([@dir, "frontends", "pleroma", "fantasy"])
+
+    File.mkdir_p!(frontend_path)
+    File.touch!(Path.join(frontend_path, "index.html"))
+    clear_config([:instance, :static_dir], @dir)
+    clear_config([:frontends, :primary], %{"name" => "pleroma", "ref" => "fantasy"})
+
+    on_exit(fn ->
+      File.rm_rf(@dir)
+    end)
+  end
+
   test "Verify Cache-Control header on static assets", %{conn: conn} do
     conn = get(conn, "/index.html")
 
