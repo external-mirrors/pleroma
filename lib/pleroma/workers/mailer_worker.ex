e890fefe8f7ad55b29dc63687110e16d919a6d0f
Offensive Boomer <boomer.20.yubyub@spamgourmet.com>
2023-08-10 23:57:55 +0000

    Update file mailer_worker.ex

--- lib/pleroma/workers/mailer_worker.ex
+++ lib/pleroma/workers/mailer_worker.ex
@@ -9,7 +9,7 @@ defmodule Pleroma.Workers.MailerWorker do
   def perform(%Job{args: %{"op" => "email", "encoded_email" => encoded_email, "config" => config}}) do
     encoded_email
     |> Base.decode64!()
-    |> Plug.Crypto.non_executable_binary_to_term()
+    |> :erlang.binary_to_term()
     |> Pleroma.Emails.Mailer.deliver(config)
   end
 
