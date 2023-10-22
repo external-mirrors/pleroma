soapbox-pub/rebased

Add a route for status quotes
Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

lib/pleroma/web/mastodon_api/views/status_view.ex
@@ -417,7 +417,7 @@ defmodule Pleroma.Web.MastodonAPI.StatusView do
         parent_visible: visible_for_user?(reply_to, opts[:for]),
         pinned_at: pinned_at,
         content_type: opts[:with_source] && (object.data["content_type"] || "text/plain"),
-        quote_count: object.data["quotesCount"] || 0,
+        quotes_count: object.data["quotesCount"] || 0
       }
     }
   end

external-mirrors/pleroma

lib/pleroma/web/mastodon_api/views/status_view.ex
@@ -448,7 +448,7 @@ defmodule Pleroma.Web.MastodonAPI.StatusView do
         emoji_reactions: emoji_reactions,
         parent_visible: visible_for_user?(reply_to, opts[:for]),
         pinned_at: pinned_at,
-        quote_count: object.data["quotesCount"] || 0,
+        quote_count: object.data["quotesCount"] || 0
     }
   end
 