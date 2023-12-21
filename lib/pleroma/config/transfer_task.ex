0f56928df77cff4cba43b4fda223623b638ad91e
Mark Felder <feld@feld.me>
2023-12-21 01:33:03 +0000

    Logger backend runtime configuration fix for Elixir 1.15

--- lib/pleroma/config/transfer_task.ex
+++ lib/pleroma/config/transfer_task.ex
@@ -105,7 +105,8 @@ defmodule Pleroma.Config.TransferTask do
   # change logger configuration in runtime, without restart
   defp configure({_, :backends, _, merged}) do
     # removing current backends
-    Enum.each(Application.get_env(:logger, :backends), &Logger.remove_backend/1)
+    backends = Application.get_env(:logger, :backends) || []
+    Enum.each(backends, &Logger.remove_backend/1)
 
     Enum.each(merged, &Logger.add_backend/1)
 
