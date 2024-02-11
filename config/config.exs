576a784547c70cdb0278239ad308a099badc8267
Mark Felder <feld@feld.me>
2024-02-11 16:53:21 -0500

    Increase the :max_body for Rich Media to 5MB
    Websites are increasingly getting more bloated with tricks like inlining content (e.g., CNN.com) which puts pages at or above 5MB. This value may still be too low.

--- config/config.exs
+++ config/config.exs
@@ -431,7 +431,8 @@ config :pleroma, :rich_media,
     Pleroma.Web.RichMedia.Parsers.OEmbed
   ],
   failure_backoff: 60_000,
-  ttl_setters: [Pleroma.Web.RichMedia.Parser.TTL.AwsSignedUrl]
+  ttl_setters: [Pleroma.Web.RichMedia.Parser.TTL.AwsSignedUrl],
+  max_body: 5_000_000
 
 config :pleroma, :media_proxy,
   enabled: false,
