f5fb4e2fa4cde903dc3f48328e3b21d0d660dfd4
Mark Felder <feld@feld.me>
2024-02-24 10:34:08 -0500

    Ignore current errors

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,5 +2,13 @@
 {"lib/cachex.ex", "Unknown type: Spec.cache/0."},
 {"lib/pleroma/web/plugs/rate_limiter.ex", "The pattern can never match the type {:commit, _} | {:ignore, _}."},
 {"lib/pleroma/web/plugs/rate_limiter.ex", "Function get_scale/2 will never be called."},
-{"lib/pleroma/web/plugs/rate_limiter.ex", "Function initialize_buckets!/1 will never be called."}
+{"lib/pleroma/web/plugs/rate_limiter.ex", "Function initialize_buckets!/1 will never be called."},
+{"lib/mix/tasks/pleroma/openapi_spec.ex", :no_return},
+{"lib/mix/tasks/pleroma/openapi_spec.ex", :unused_fun},
+{"lib/pleroma/user/backup.ex", :pattern_match},
+{"lib/pleroma/web/api_spec.ex", :no_return},
+{"lib/pleroma/web/api_spec.ex", :call},
+{"lib/pleroma/web/api_spec/scopes/compiler.ex", :no_return},
+{"lib/pleroma/web/o_auth/mfa_controller.ex", :pattern_match},
+{"lib/pleroma/web/o_auth/o_auth_controller.ex", :pattern_match}
 ]
