6702b2b7f9a08833e4e414a0991456537aa90fa4
Mark Felder <feld@feld.me>
2024-06-04 22:22:41 -0400

    Formatting

--- lib/pleroma/chaos.ex
+++ lib/pleroma/chaos.ex
@@ -1,6 +1,6 @@
 defmodule Pleroma.Chaos do
   def kill_random_worker(pid) do
-      with true <- Enum.random(0..100) >= 75 do
+    with true <- Enum.random(0..100) >= 75 do
       Process.exit(pid, :kill)
     end
   end
