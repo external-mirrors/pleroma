e94d597506120fb0d45928ea19b1f8892e3c4dca
Mark Felder <feld@feld.me>
2025-04-04 18:57:13 -0700

    Fix warning about missing parens

--- lib/pleroma/language/language_detector.ex
+++ lib/pleroma/language/language_detector.ex
@@ -41,7 +41,7 @@ defmodule Pleroma.Language.LanguageDetector do
     text = prepare_text(text)
     word_count = text |> String.split(~r/\s+/) |> Enum.count()
 
-    if word_count < @words_threshold or !provider or !provider.configured? do
+    if word_count < @words_threshold or !provider or !provider.configured?() do
       nil
     else
       with language <- provider.detect(text),
