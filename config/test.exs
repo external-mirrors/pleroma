ae7e07654d5aa615ab96f7570dc4b1ddf55798e9
Lain Soykaf <lain@lain.com>
2025-06-20 13:10:45 +0400

    config/test.exs: Fix typo

--- config/test.exs
+++ config/test.exs
@@ -188,7 +188,7 @@ config :pleroma, Pleroma.Application,
   streamer_registry: false,
   test_http_pools: true
 
-config :pleroma, Pleroma.Web.Streaming, sync_streaming: true
+config :pleroma, Pleroma.Web.Streamer, sync_streaming: true
 
 config :pleroma, Pleroma.Uploaders.Uploader, timeout: 1_000
 
